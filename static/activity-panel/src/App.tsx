1f755ff18a49efc4c850eed014f3071acc6bf1cd
maciej.kuster <maciej.kuster@dynatrace.com>
2026-03-31 14:24:50 +0200

    Add NoTenantsConfigured component and update App to handle tenant configurations

--- static/activity-panel/src/App.tsx
+++ static/activity-panel/src/App.tsx
@@ -1,9 +1,25 @@
 import { IssuePanel } from './components/IssuePanel.tsx';
+import { useTenantConfigs } from './hooks/useTenantConfigs.tsx';
+import { NoTenantsConfigured } from './components/errors/NoTenantsConfigured.tsx';
 
 function App() {
+  const { tenantConfigs, isLoading } = useTenantConfigs();
+
+  const renderContent = () => {
+    if (isLoading) {
+      return <div className="p-4 text-gray-800 dark:text-gray-200">Loading...</div>;
+    }
+
+    if (tenantConfigs.length === 0) {
+      return <NoTenantsConfigured />;
+    }
+
+    return <IssuePanel />;
+  };
+
   return (
     <div className="min-h-[600px] p-4 space-y-4 bg-white dark:bg-gray-900 transition-colors">
-      <IssuePanel />
+      {renderContent()}
     </div>
   );
 }
